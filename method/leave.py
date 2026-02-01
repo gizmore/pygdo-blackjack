0a76c4c8363978723ad81d2c24090204fe9c04ba
gizmore <gizmore@wechall.net>
2026-02-01 07:50:42 +0100

    'bj'

---
+++ method/leave.py
@@ -0,0 +1,27 @@
+from gdo.base.GDT import GDT
+from gdo.base.Method import Method
+from gdo.base.Util import module_config_value, Files, get_module
+import hashlib
+
+class leave(Method):
+
+        @classmethod
+        def gdo_trigger(cls) -> str:
+            return 'bj.leave'
+
+        def gdo_execute(self) -> GDT:
+            score_have = self._env_user.get_setting_value('bj_credits')
+            score_required = module_config_value('blackjack', 'bj_millionaire')
+            if score_have < score_required:
+                return self.err('err_bj_millionaire', (score_required, score_have, score_required - score_have))
+            solution = self.get_solution_blackjack(self._env_user.render_name(), Files.get_contents(get_module('blackjack').file_path('password.txt')))
+            return self.msg('msg_bj_millionaire', (score_required, solution))
+
+        def md5_hex(self, s: str) -> str:
+            return hashlib.md5(s.encode("utf-8")).hexdigest()
+
+        def get_solution_blackjack(self, playername: str, dog_password: str) -> str:
+            pname = playername.lower()
+            h = self.md5_hex(dog_password + self.md5_hex(pname) + dog_password)[1:17]
+            return f"{pname}!{h}!rich"
+        
--- module_blackjack.py
+++ module_blackjack.py
@@ -20,10 +20,14 @@ class module_blackjack(GDO_Module):
             GDT_UInt('bj_games_lost').initial('0').writable(False),
             GDT_Credits('bj_money_won').initial('0').writable(False),
             GDT_Credits('bj_money_lost').initial('0').writable(False),
+            GDT_Credits('bj_millionaire').initial('64000').min(0).max(1_000_000),
         ]
 
     def cfg_min_bet(self) -> int:
         return self.get_config_value('bj_min_bet')
+
+    def cfg_millionaire_score(self) -> int:
+        return self.get_config_value('bj_millionaire')
 
     def gdo_init_sidebar(self, page):
         page._left_bar.add_field(GDT_Link().href(self.href('site')).text('module_blackjack'))
